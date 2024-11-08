9a364333a8c391f0dd78732cd3e0f82bc8bb3e13
Ricardo Araújo <ricardoaraujo@MacBook-Pro-de-Ricardo.local>
2024-11-08 16:06:18 +0000

    Correção de bugs

--- src/vending/MenuColaborador.java
+++ src/vending/MenuColaborador.java
@@ -29,7 +29,49 @@ public class MenuColaborador {
 
             switch (opcao) {
                 case 1:
-                    // Lógica para adicionar produto
+
+                    System.out.println("Escolha o tipo de produto (1: Chocolate, 2: Refrigerante, 3: Sande):");
+                    int tipoProduto = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("Nome do produto:");
+                    String nome = scanner.nextLine();
+                    System.out.println("Preço:");
+                    double preco = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.println("Referência:");
+                    String referencia = scanner.nextLine();
+                    System.out.println("Prazo de validade:");
+                    String prazoValidade = scanner.nextLine();
+
+                    Produto novoProduto = null;
+
+                    if (tipoProduto == 1) {
+                        System.out.println("Tipo de Cacau (1: Branco || 2: Ao leite || 3: Negro)");
+                        int tipoCacau = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("Marca:");
+                        String marca = scanner.nextLine();
+                        novoProduto = new Chocolate(nome, preco, referencia, prazoValidade, tipoCacau, marca);
+                    } else if (tipoProduto == 2) {
+                        System.out.println("Tipo (1: Normal || 2: Sem açúcar)");
+                        int tipo = scanner.nextInt();
+                        System.out.println("Marca:");
+                        String marca = scanner.nextLine();
+                        novoProduto = new Refrigerante(nome, preco, referencia, prazoValidade, tipo, marca);
+                    } else if (tipoProduto == 3) {
+                        System.out.println("Tipo (1: Queijo || 2: Fiambre || 3: Mista):");
+                        int tipo = scanner.nextInt();
+                        System.out.println("Nome do Produtor:");
+                        String nomeProdutor = scanner.nextLine();
+                        novoProduto = new Sande(nome, preco, referencia, prazoValidade, tipo, nomeProdutor);
+                    }
+
+                    if (novoProduto != null) {
+                        colaborador.adicionarProduto(maquina, novoProduto);
+                    }
+                    maquina.salvarDados();
+
                     break;
                 case 2:
                     System.out.println("Histórico de Vendas:");
